5446dabe2d3026611bbd8d37d67d126673efc43e
AvaSama <kevin.voss99@gmail.com>
2021-11-23 08:23:05 +0100

    validate frontend

---
+++ frontend/src/utils/validate.js
@@ -0,0 +1,17 @@
+export const validateEmail = (email) => {
+  return new RegExp(
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  ).test(email);
+};
+
+export const validateUsername = (username) => {
+  return new RegExp(
+    /^(?=.{3,30}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/
+  ).test(username);
+};
+
+export const validatePassword = (password) => {
+  return new RegExp(
+    /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,30}$/
+  ).test(password);
+};
